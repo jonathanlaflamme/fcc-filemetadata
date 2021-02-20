ab695c0ee4f5e19b4f8e2cc22b67d12214737d04
Jonathan Laflamme <jonathan.laflamme.jl@gmail.com>
2021-02-20 10:23:07 -0500

    done

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
 
 var app = express();
 
@@ -8,10 +10,13 @@ app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const { file } = req;
+  res.json({ "name": file.originalname, "type": file.mimetype, "size": file.size })
+})
 
 
 const port = process.env.PORT || 3000;
